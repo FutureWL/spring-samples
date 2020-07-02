b027c1c0ad0373392092221d2e76ecce37a92593
FutureWL <624263934@qq.com>
2020-07-02 21:34:15 +0800

    更新：Thymeleaf

---
+++ spring-boot-samples-web/spring-boot-samples-web-template/spring-boot-samples-web-template-thymeleaf/src/main/java/io/github/futurewl/web/template/AppWebTemplateThymeleaf.java
@@ -0,0 +1,11 @@
+package io.github.futurewl.web.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebTemplateThymeleaf {
+    public static void main(String[] args) {
+        SpringApplication.run(AppWebTemplateThymeleaf.class, args);
+    }
+}
