d79b20fda8f480bfc77036212b9051adef00709f
FutureWL <624263934@qq.com>
2020-09-20 17:25:59 +0800

    更新

---
+++ spring-boot-samples/spring-boot-samples-web/spring-boot-samples-web-server/spring-boot-samples-web-server-tomcat/src/main/java/cn/huntercat/SpringBootWebTomcat.java
@@ -0,0 +1,11 @@
+package cn.huntercat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebTomcat {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebTomcat.class, args);
+    }
+}
