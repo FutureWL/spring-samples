8b7bdc48b7c319441d4421b24cc6b9d5f777d83c
FutureWL <624263934@qq.com>
2020-09-21 10:41:57 +0800

    更新：增加 ELK 案例

--- spring-data-samples/spring-data-samples-elasticsearch/src/main/java/io/github/futurewl/data/AppDataElasticsearch.java
+++ spring-data-samples/spring-data-samples-elasticsearch/src/main/java/io/github/futurewl/data/AppDataElasticsearch.java
@@ -1,7 +1,22 @@
 package io.github.futurewl.data;
 
-public class AppDataElasticsearch {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：
+ */
+@SpringBootApplication
+public class AppDataElasticsearch implements CommandLineRunner {
+
+
     public static void main(String[] args) {
+        SpringApplication.run(AppDataElasticsearch.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
 
     }
 }
